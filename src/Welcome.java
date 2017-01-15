2d07e786c622a8fc86702525fb73f76553c21cb6
qwerty <tinhcx@gmail.com>
2017-01-15 08:50:34 +0700

    Uploaded Welcome.java

---
+++ src/Welcome.java
@@ -0,0 +1,34 @@
+/**
+ * #######################################
+ * ProjectName: JavaSE
+ * PackageName: 
+ * FileName: Welcome.java
+ * GitHub: https://github.com/ToiHocJava/JavaSE
+ * GitInfo: qwerty | tinhcx@gmail.com
+ * CreatedTime: Jan 15, 2017 | 8:47:47 AM
+ * #######################################
+ */
+
+/**
+ * @author w
+ *
+ */
+public class Welcome {
+
+	/**
+	 * 
+	 */
+	public Welcome() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Javaworld");
+
+	}
+
+}

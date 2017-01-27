8bf60eda527752b78993a3953257792752049bef
qwerty <tinhcx@gmail.com>
2017-01-27 07:34:03 +0700

    1

--- src/chapter02/ImageViewer.java
+++ src/chapter02/ImageViewer.java
@@ -1,97 +1,97 @@
-/**
- * #######################################
- * ProjectName: JavaSE
- * PackageName: chapter02
- * FileName: ImageViewer.java
- * GitHub: https://github.com/ToiHocJava/JavaSE
- * GitInfo: qwerty | tinhcx@gmail.com
- * CreatedTime: Jan 21, 2017 | 6:44:25 AM
- * #######################################
- */
-package chapter02;
-
-import java.awt.MenuBar;
-import java.io.File;
-
-import javax.swing.ImageIcon;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-
-import com.sun.javafx.event.EventQueue;
-
-/**
- * @author w
- *
- */
-public class ImageViewer {
-
-	/**
-	 * 
-	 */
-	public ImageViewer() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		java.awt.EventQueue.invokeLater(() -> {
-			J
-		}	
-		);
-
-	}
-	/**
-	 * A frame with a label to show an image
-	 */
-	class ImageViewerFrame extends JFrame
-	{
-		private JLabel label;
-		private JFileChooser chooser;
-		private static final int DEFAULT_WIDTH=300;
-		private static final int DEFAULT_HEIGHT=400;
-		
-		public ImageViewerFrame()
-		{
-			setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-			
-//			Use a label to display the images;
-			label = new JLabel();
-			add(label);
-			
-//			setup a file choose;
-			chooser =  new JFileChooser();
-			chooser.setCurrentDirectory(new File("."));
-			
-//			setup the menu bar
-			JMenu menuBar =  new JMenu("File");
-			setJMenuBar(menuBar);
-			
-			JMenu menu = new JMenu("File");
-			menuBar.add(menu);
-			
-			JMenuItem openItem = new JMenuItem("Open");
-			menu.add(openItem);
-			
-			openItem.addActionListener(event -> {
-//				show file chooser dialog;
-				int result = chooser.showOpenDialog(null);
-				
-//				if file selected, set it as icon of the label
-				if (result == JFileChooser.APPROVE_OPTION) {
-					String name = chooser.getSelectedFile().getPath();
-					label.setIcon(new ImageIcon(name));
-				}				
-			});
-			//
-			
-			
-		}
-	}
-
-}
+///**
+// * #######################################
+// * ProjectName: JavaSE
+// * PackageName: chapter02
+// * FileName: ImageViewer.java
+// * GitHub: https://github.com/ToiHocJava/JavaSE
+// * GitInfo: qwerty | tinhcx@gmail.com
+// * CreatedTime: Jan 21, 2017 | 6:44:25 AM
+// * #######################################
+// */
+//package chapter02;
+//
+//import java.awt.MenuBar;
+//import java.io.File;
+//
+//import javax.swing.ImageIcon;
+//import javax.swing.JFileChooser;
+//import javax.swing.JFrame;
+//import javax.swing.JLabel;
+//import javax.swing.JMenu;
+//import javax.swing.JMenuItem;
+//
+//import com.sun.javafx.event.EventQueue;
+//
+///**
+// * @author w
+// *
+// */
+//public class ImageViewer {
+//
+//	/**
+//	 * 
+//	 */
+//	public ImageViewer() {
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//	/**
+//	 * @param args
+//	 */
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		java.awt.EventQueue.invokeLater(() -> {
+//			J
+//		}	
+//		);
+//
+//	}
+//	/**
+//	 * A frame with a label to show an image
+//	 */
+//	class ImageViewerFrame extends JFrame
+//	{
+//		private JLabel label;
+//		private JFileChooser chooser;
+//		private static final int DEFAULT_WIDTH=300;
+//		private static final int DEFAULT_HEIGHT=400;
+//		
+//		public ImageViewerFrame()
+//		{
+//			setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+//			
+////			Use a label to display the images;
+//			label = new JLabel();
+//			add(label);
+//			
+////			setup a file choose;
+//			chooser =  new JFileChooser();
+//			chooser.setCurrentDirectory(new File("."));
+//			
+////			setup the menu bar
+//			JMenu menuBar =  new JMenu("File");
+//			setJMenuBar(menuBar);
+//			
+//			JMenu menu = new JMenu("File");
+//			menuBar.add(menu);
+//			
+//			JMenuItem openItem = new JMenuItem("Open");
+//			menu.add(openItem);
+//			
+//			openItem.addActionListener(event -> {
+////				show file chooser dialog;
+//				int result = chooser.showOpenDialog(null);
+//				
+////				if file selected, set it as icon of the label
+//				if (result == JFileChooser.APPROVE_OPTION) {
+//					String name = chooser.getSelectedFile().getPath();
+//					label.setIcon(new ImageIcon(name));
+//				}				
+//			});
+//			//
+//			
+//			
+//		}
+//	}
+//
+//}
--- tutorial/Chapter01-02-Exercices.md
+++ tutorial/Chapter01-02-Exercices.md
@@ -2,9 +2,66 @@
 ##########################
 
 **Bài tập cho chương 1,2 (20170122 17:12:56)**
+
 > 1. Thế nào là JDK, JRE? Dùng JDK hoặc JRE trong những trường hợp nào?
 > 2. Các biến môi trường PATH và CLASSPATH là gì? và được ứng dụng trong thực tế (qua nhiều hệ điều hành Windows, Linux ...) như thế nào?
 > 3. Làm thế nào chỉ cần 1 câu lệnh có thể thực thi 1 file chứa source java được soạn từ console hoặc notepad?
 > 4. Làm thế nào để tạo ra một bản protable JAVA có thể chạy mà không cần cài đặt? (đa phần các ứng dụng Java đều có yêu cầu cài sẵn môi trường Java)
 > 5. Theo quan điểm riêng của bạn, vì sao Java đang đứng top 1 của các ngôn ngữ lập trình? (theo Tiobe)
 mọi người trả lời trên GITHUB và paste link lên đây nhé.
+
+
+
+
+**Answer (7:13 20170127)**
+> 1. Thế nào là JDK, JRE? Dùng JDK hoặc JRE trong những trường hợp nào?
+
+
+<ul>
+    <li>
+    JDK is "Java Development Kit". That's the software for PROGRAMMERS who want to write Java programs.
+    </li>
+
+    <li>
+    JRE is "Java Runtime Environment". That's the software for consumers who want to run Java programs.
+    </li>
+
+    <li>
+    </li>
+
+    <li>
+    </li>
+
+    <li>
+    </li>
+</ul>
+#
+
+
+
+
+
+
+
+
+
+
+<ul>
+    <li>
+    JDK is "Java Development Kit". That's the software for PROGRAMMERS who want to write Java programs.
+    </li>
+
+    <li>
+    
+    </li>
+
+    <li>
+    </li>
+
+    <li>
+    </li>
+
+    <li>
+    </li>
+</ul>
+#

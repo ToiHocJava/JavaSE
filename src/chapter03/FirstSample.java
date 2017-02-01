e3fae5406155cf827decef54bd572f605ac58bd2
qwerty <tinhcx@gmail.com>
2017-02-01 17:17:57 +0700

    3

---
+++ src/chapter03/FirstSample.java
@@ -0,0 +1,35 @@
+/**
+ * #######################################
+ * ProjectName: JavaSE
+ * PackageName: chapter03
+ * FileName: FirstSample.java
+ * GitHub: https://github.com/ToiHocJava/JavaSE
+ * GitInfo: qwerty | tinhcx@gmail.com
+ * CreatedTime: Feb 1, 2017 | 5:17:10 PM
+ * #######################################
+ */
+package chapter03;
+
+/**
+ * @author w
+ *
+ */
+public class FirstSample {
+
+	/**
+	 * 
+	 */
+	public FirstSample() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("We wil not use 'Hello, World!'");
+
+	}
+
+}
